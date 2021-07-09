3cc13bf74c35ae3befc58cca6ed1ffdaf9a2dfc1
Your Name <sabeel.shaik@rapyuta-robotics.com>
2021-07-09 14:53:39 +0530

    rapyuta

--- talk/talker/talker.py
+++ talk/talker/talker.py
@@ -8,7 +8,7 @@ def talker():
 	rospy.init_node('talker', anonymous=True)
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
-		hello_str = "rapyuta hello_world %s" % rospy.get_time()
+		hello_str = "rapyuta new hello_world %s" % rospy.get_time()
 		rospy.loginfo(hello_str)
 		pub.publish(hello_str)
 		rate.sleep()
